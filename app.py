3bf540e6eec0873ef07bde492c9bcb09f0eae656
YunusEmreCelik06 <yunusemrecelikwsb@gmail.com>
2026-03-13 00:38:05 +0100

    doviz sitesi guncellendi

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('MY_SECRET_KEY', 'gelistirici-gizli-key')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
